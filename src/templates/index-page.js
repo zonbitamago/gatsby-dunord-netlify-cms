e9a125718ca4701b4e38c2b09e105ca0e437c537
kazuma takiuchi <basstrombone413@gmail.com>
2020-01-27 22:26:52 +0900

    change top image

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -22,8 +22,8 @@ export const IndexPageTemplate = ({
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
         })`,
-        backgroundPosition: `center`,
-        backgroundAttachment: `fixed`
+        backgroundPosition: `center`
+        // backgroundAttachment: `fixed`
       }}
     >
       <div
