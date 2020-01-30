12b7fd905ae06efad606bb97aba5b8adfcd5723e
kazuma takiuchi <basstrombone413@gmail.com>
2020-01-30 22:14:21 +0900

    AddFeaturedPost

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,7 +4,7 @@ import { Link, graphql } from "gatsby";
 
 import Layout from "../components/Layout";
 // import Features from "../components/Features";
-// import BlogRoll from "../components/BlogRoll";
+import FeaturedRoll from "../components/FeaturedRoll";
 
 import { TwitterTimelineEmbed, TwitterFollowButton } from "react-twitter-embed";
 import { FacebookProvider, Page } from "react-facebook";
@@ -91,13 +91,24 @@ export const IndexPageTemplate = ({
                     <p>{description}</p>
                   </div>
                 </div>
-                <div className="columns">
+                <div className="column is-12">
+                  {/* <h3 className="has-text-weight-semibold is-size-2">
+                    Information
+                  </h3> */}
+                  <FeaturedRoll />
+                  <div className="column is-12 has-text-centered">
+                    <Link className="btn" to="/blog">
+                      More Information
+                    </Link>
+                  </div>
+                </div>
+                {/* <div className="columns">
                   <div className="column is-12 has-text-centered">
                     <Link className="btn_bg" to="/blog">
                       Information
                     </Link>
                   </div>
-                </div>
+                </div> */}
                 {/* <Features gridItems={intro.blurbs} /> */}
                 <div className="columns is-multiline">
                   <div className="column is-6">
@@ -143,18 +154,6 @@ export const IndexPageTemplate = ({
                     </section>
                   </div>
                 </div>
-
-                {/* <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div> */}
               </div>
             </div>
           </div>
