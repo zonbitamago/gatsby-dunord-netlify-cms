4556a7094205ab5778ded682501efebc0458a594
kazuma takiuchi <basstrombone413@gmail.com>
2020-02-06 05:22:24 +0900

    fix description

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,7 +9,7 @@ import Content, { HTMLContent } from "../components/Content";
 export const BlogPostTemplate = ({
   content,
   contentComponent,
-  description,
+  // description,
   tags,
   title,
   helmet
@@ -25,7 +25,7 @@ export const BlogPostTemplate = ({
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
               {title}
             </h1>
-            <p>{description}</p>
+            {/* <p>{description}</p> */}
             <PostContent content={content} />
             {tags && tags.length ? (
               <div style={{ marginTop: `4rem` }}>
@@ -49,7 +49,7 @@ export const BlogPostTemplate = ({
 BlogPostTemplate.propTypes = {
   content: PropTypes.node.isRequired,
   contentComponent: PropTypes.func,
-  description: PropTypes.string,
+  // description: PropTypes.string,
   title: PropTypes.string,
   helmet: PropTypes.object
 };
@@ -62,7 +62,7 @@ const BlogPost = ({ data }) => {
       <BlogPostTemplate
         content={post.html}
         contentComponent={HTMLContent}
-        description={post.frontmatter.description}
+        // description={post.frontmatter.description}
         helmet={
           <Helmet titleTemplate="%s | Information">
             <title>{`${post.frontmatter.title}`}</title>
