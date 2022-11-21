e562c6aa50a85d27f8aac7e98e872c0a7e366758
kazuma takiuchi <basstrombone413@gmail.com>
2022-11-21 22:18:30 +0900

    js to tsx

--- gatsby-node.js
+++ gatsby-node.js
@@ -68,7 +68,7 @@ exports.createPages = ({ actions, graphql }) => {
 
       createPage({
         path: tagPath,
-        component: path.resolve(`src/templates/tags.js`),
+        component: path.resolve(`src/templates/tags.tsx`),
         context: {
           tag,
         },
